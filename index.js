69374008c17ce034df0a7ad5d727b93f6f9a0e12
Adrián Naranjo <adriannaranjosanchez@gmail.com>
2020-10-31 00:06:17 +0100

    Dia 2 HTML | CSS | JS

--- index.js
+++ index.js
@@ -15,10 +15,80 @@ function httpGetAsync(url, callback) {
   xmlHttp.send(null);
 }
 
+function printMovies(parentHTML, movies) {
+  parentHTML.innerHTML = "";
+
+  movies.forEach((movie) => {
+    parentHTML.appendChild(printMovie(movie));
+    // Añadir al padre
+    // listMoviesContainer.appendChild(liMovie);
+  });
+}
+
+function printMovie(movie) {
+  // declaracion elemento html
+  let liMovie = document.createElement("li");
+
+  // DIV CONTENEDOR
+  let movieContainer = document.createElement("div");
+  movieContainer.className = "movie__container";
+
+  // DIV CONTENEDOR IMAGEN
+  let movieImageContainer = document.createElement("div");
+  movieImageContainer.className = "movie__image__container";
+
+  // IMAGEN
+  let movieImage = document.createElement("img");
+  movieImage.src = movie["Poster"];
+
+  movieImageContainer.appendChild(movieImage);
+
+  movieContainer.appendChild(movieImageContainer);
+
+  // DIV CONTENEDOR INFO
+
+  let movieInfoContainer = document.createElement("div");
+  movieInfoContainer.className = "movie__info__container";
+
+  // TITULO
+  let movieTitle = document.createElement("h1");
+  movieTitle.textContent = movie["Title"];
+
+  movieInfoContainer.appendChild(movieTitle);
+
+  // HR
+  let movieHr = document.createElement("hr");
+  movieInfoContainer.appendChild(movieHr);
+
+  // AÑO
+  let movieYear = document.createElement("h2");
+  movieYear.textContent = `Año: ${movie["Year"]}`;
+
+  movieInfoContainer.appendChild(movieYear);
+
+  // TIPO
+  let movieType = document.createElement("p");
+  movieType.textContent = `Tipo: ${movie["Type"]}`;
+
+  movieInfoContainer.appendChild(movieType);
+
+  movieContainer.appendChild(movieInfoContainer);
+
+  liMovie.appendChild(movieContainer);
+
+  return liMovie;
+}
+
 function getMovies(movies) {
   console.log(movies);
   movies = JSON.parse(movies);
   console.table(movies["Search"]);
+
+  const listMoviesContainer = document.getElementById(
+    "moviesSearchListContainer"
+  );
+
+  printMovies(listMoviesContainer, movies["Search"]);
 }
 
 function searchMovie() {
